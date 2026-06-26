d9c28f65d13574f8482857fbd69946188ffb282e
blackifiedbro <izazfahrizaakbar@gmail.com>
2026-06-27 00:54:39 +0700

    Add files via upload

---
+++ Python/Day01_Strings/#Roblox_Profile_Card.py
@@ -0,0 +1,12 @@
+#Roblox Profile Card
+
+print("====================")
+print(" ROBLOX PROFILE")
+print("====================")
+print() 
+username = input("Username: ")
+fav_game = input("Favorite Roblox Game: ")
+fav_color = input("Favorite Color: ")
+print()
+print(f"Have fun playing {fav_game} {username.upper()}!")
+print(f"Fun fact, your name has {len(username)} characters in it! And your favorite color is {fav_color.lower()}!")
